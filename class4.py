8a125163c8e3a83c9d2b1304d2a406f753d66ed2
Camila Marques <mfs.cami@gmail.com>
2024-06-18 21:31:53 -0300

    Add files via upload

---
+++ class4.py
@@ -0,0 +1,22 @@
+produto_1 = 20
+produto_2 = 10
+
+print(produto_1 + produto_2)
+print(produto_1 - produto_2)
+print(produto_1 / produto_2)
+print(produto_1 // produto_2)
+print(produto_1 * produto_2)
+print(produto_1 ** produto_2)
+print(produto_1 % produto_2)
+
+x = (10 + 5) * 4
+print(x)
+
+y = 10 + 5 * 4
+print(y)
+
+a = 10 / 2 + 25 * 2 - (2 ** 2)
+print(a)
+
+b = (10 / 2) + 25 * ((2 - 2) ** 2)
+print(b)
